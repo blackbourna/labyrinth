f20fe43f76e42d6a8a27b7f04a3fb9edb18cb056
Andrew <andrew@andrew-mint.(none)>
2012-11-30 14:52:44 -0500

    frame rate thing done

--- soft_eng/game.js
+++ soft_eng/game.js
@@ -65,13 +65,13 @@ soft_eng.Game = function(director, level) {
 	prevAcceleration = {};
     var world = new b2World(new b2Vec2(0, 0), true);
     
-    var device = navigator.userAgent.toLowerCase();
+    var deviceName = navigator.userAgent.toLowerCase();
     
     var FRAME_RATE = 18;
     
-    if (device.indexOf('nexus 7' > -1)) {
+    if (deviceName.indexOf('nexus 7') > -1) {
         FRAME_RATE = 18;
-    } else if (device.indexOf('samsung-sgh' > -1)) {
+    } else if (deviceName.indexOf('samsung-sgh') > -1) {
         FRAME_RATE = 45;
     }
     
